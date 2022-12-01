d3b930ba2d627ca0e6dbe74edeff9bfcf67037d8
masterandroide <scalderon.info@gmail.com>
2022-12-01 18:14:52 -0300

    ruta test

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ const app = express();
 
 
 app.get('/test', function (req, res) {
-    res.json({resp: 'Saludos desde api de prueba para azure'});
+    res.json({resp: 'Saludos desde ruta test'});
   });
+
+
+app.get('/', function (req, res) {
+  res.json({resp: 'Saludos desde api de prueba para azure'});
+});
 
 
 app.listen(3000, () => {
